dd1af514169017ac30d040a837fca06a07186267
Abinet-Bushura <abintbushura4@gmail.com>
2022-03-11 06:41:38 -1200

    abint bushraaa

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,6 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
-/* betty style doc for function main goes there */
+
 /**
  * main - Entry point
  *
@@ -10,37 +8,14 @@
  */
 int main(void)
 {
-	int i;
-	int j;
+	char l;
 
-	i = 48;
-	j = 48;
-	while  ((i < 58) && (j < 58))
+	l = 'a';
+	while (l <= 'z')
 	{
-		putchar(i);
-		putchar(j);
-		if ((i == 57) && (j == 57))
-		{
-			putchar('\n');
-			i++;
-			j++;
-		}
-		else
-		{
-			putchar(44);
-			putchar(32);
-			if (j < 57)
-			{
-				j++;
-			}
-			else
-			{
-				j = 48;
-				i++;
-			}
-		}
-
+		putchar(l);
+		l++;
 	}
-
+	putchar('\n');
 	return (0);
 }
