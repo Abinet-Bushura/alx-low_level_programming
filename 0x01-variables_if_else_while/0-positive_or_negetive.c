244b561b864e4de15ac683816bfc6c6170794ab1
Abinet-Bushura <nahomtemesgen@gmail.comgit/>
2022-04-08 13:16:22 -1200

    my commit message

---
+++ 0x01-variables_if_else_while/0-positive_or_negetive.c
@@ -0,0 +1,32 @@
+#Include <stdlib.h>
+Include[D[D[D[D[D[D[D[D[D[D#[C[C[C[C[C[C[C[C<time.h.
+[A[CI[C[C[C[C[C[C[C[C[C[C[C[C[C[C[>
+#include[D[D[D[D[D[I[C[C[C[C[C[C[C<stdio.h>
+
+
+[A[A[B[A[A[B[B/**
+*main -Entry point
+*
+*Retun: always 0(success/correct)
+*/
+
+Int main(void)
+{
+int n:srand(time(0)):
+n =rand() -RAND_MAX /2:
+if (n==0)
+{
+printf("%i is zero[1;2o/n",n):
+}
+else if (n<0){printf("[D[D[D[D[D[D[D[D
+[D[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C%i is negative /n", n);
+}
+
+else 
+{
+printf("%i is positive/n", n);[A[A[A[A[A;[A[A;;[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[C[C[C[C[C[C[C[C[C
+}
+
+return (0);
+
+}
